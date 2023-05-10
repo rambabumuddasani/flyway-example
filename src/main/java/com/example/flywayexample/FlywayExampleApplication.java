ee28568d44f6b0a110d20053e085cdeed65ee9a0
ram <rambabu0006@gmail.com>
2023-05-11 00:51:20 +0530

    flyway db basic project setup

---
+++ src/main/java/com/example/flywayexample/FlywayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywayexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayExampleApplication.class, args);
+	}
+
+}
